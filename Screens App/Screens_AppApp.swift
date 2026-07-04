e5a85114a0afba61fe1219a64368041417cd8e67
Gabriel <gabrielthamls12@gmail.com>
2026-07-04 14:23:13 +0800

    Initial Commit

---
+++ Screens App/Screens_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Screens_AppApp.swift
+//  Screens App
+//
+//  Created by Gabriel on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Screens_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
